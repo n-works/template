954a714f49cc7ee68bb2cc598308b65a18800d09
inamori-n-works <55476224+inamori-n-works@users.noreply.github.com>
2020-02-02 16:25:26 +0900

    @n-works/create-local v1.0.4 (#9)
    ### create-local
    * 雛形ファイル導入 (.template)
    * prompts インストール (プロジェクト名を対話式で入力可能に)
    * fs-extra アンインストール (再帰コピーを独自実装)
    * yarn upgrade
    
    ### テンプレート
    * アセットビルダーを @n-works/asset-builder v1.0.1 に差し替え
    * @babel/core アンインストール (Parcel のデフォルト)
    * .babelrc から @babel/preset-env 削除 (Parcel のデフォルト)
    * .env.assets から ASSETS_PATH_SERVER 削除
    * README.md 修正
    * .editorconfig 追加
    * .gitattributes 追加

--- packages/local/index.js
+++ packages/local/index.js
@@ -1,25 +1,50 @@
 #!/usr/bin/env node
 
 const path = require('path')
-const fs = require('fs-extra')
+const fs = require('fs')
+const prompts = require('prompts')
 
-const projectName = path.basename(path.resolve('.'))
-const basePath = path.join(__dirname, 'template')
+// 再帰的にテンプレートファイルをコピー
+function copyTemplateFiles (srcPath, distPath, projectName) {
+  fs.readdirSync(srcPath, { withFileTypes: true })
+    .forEach(dirent => {
+      const src = path.join(srcPath, dirent.name)
 
-fs.readdirSync(basePath, { withFileTypes: true })
-  .forEach(dirent => {
-    const srcPath = path.join(basePath, dirent.name)
-    const distPath = path.join(path.resolve('.'), dirent.name)
+      // 雛形ファイルの拡張子 .template は取り除く
+      const dist = path.join(distPath, dirent.name.replace(/\.template$/, ''))
 
-    fs.copySync(srcPath, distPath, { overwrite: false })
+      if (dirent.isDirectory()) {
+        if (!fs.existsSync(dist)) {
+          fs.mkdirSync(dist, { recursive: true })
+        }
+        copyTemplateFiles(src, dist, projectName)
+      } else {
+        if (!fs.existsSync(dist)) {
+          fs.copyFileSync(src, dist)
 
-    if (dirent.name === '.env.assets' || dirent.name === 'README.md') {
-      fs.writeFileSync(distPath, fs.readFileSync(distPath, 'utf-8')
-        .replace(/{{ PROJECT_NAME }}/g, projectName), 'utf-8')
-    }
+          // 雛形ファイルにプロジェクト名を書き込み
+          if (dirent.name.match(/\.template$/)) {
+            const text = fs.readFileSync(dist, 'utf-8')
+              .replace(/{{ PROJECT_NAME }}/g, projectName)
+
+            fs.writeFileSync(dist, text, 'utf-8')
+          }
+        }
+      }
+    })
+}
+
+(async () => {
+  const response = await prompts({
+    type: 'text',
+    name: 'projectName',
+    message: 'Please enter this project name',
+    initial: path.basename(path.resolve('.'))
   })
 
-const envfilePath = path.join(path.resolve('.'), '.env')
-if (!fs.existsSync(envfilePath)) {
-  fs.writeFileSync(envfilePath, `COMPOSE_PROJECT_NAME=${projectName}\n`, 'utf-8')
-}
+  copyTemplateFiles(
+    path.join(__dirname, 'template'),
+    path.resolve('.'),
+    response.projectName
+  )
+})()
